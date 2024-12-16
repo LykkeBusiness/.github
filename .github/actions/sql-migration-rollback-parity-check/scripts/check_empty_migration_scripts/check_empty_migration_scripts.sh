099e6398109dcc8c1f3b551ddcbbcbdeca790bd0
atarutin <andrey.tarutin@lykke-business.ch>
2024-12-16 17:33:02 +0300

    fix: update bash script options for improved error handling and formatting

--- .github/actions/sql-migration-rollback-parity-check/scripts/check_empty_migration_scripts/check_empty_migration_scripts.sh
+++ .github/actions/sql-migration-rollback-parity-check/scripts/check_empty_migration_scripts/check_empty_migration_scripts.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-set -Eeuo pipefail
+set -Eeuox pipefail
 
 if [ ! -f migration_scripts.txt ] || [ ! -s migration_scripts.txt ]; then
     echo "No migration script files found."
     # Set empty validation result
-    echo "validation-result=" >> "$GITHUB_OUTPUT"
+    echo "validation-result=" >>"$GITHUB_OUTPUT"
     exit 0
 fi
