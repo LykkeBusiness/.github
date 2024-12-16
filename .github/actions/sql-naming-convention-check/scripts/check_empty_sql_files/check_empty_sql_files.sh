222c83f685bdf6e4e68b2b14b1376789e431b699
atarutin <andrey.tarutin@lykke-business.ch>
2024-12-16 18:48:30 +0300

    fix: enhance SQL naming convention check by adding conditional execution based on changed files

--- .github/actions/sql-naming-convention-check/scripts/check_empty_sql_files/check_empty_sql_files.sh
+++ .github/actions/sql-naming-convention-check/scripts/check_empty_sql_files/check_empty_sql_files.sh
@@ -4,6 +4,8 @@ set -Eeuo pipefail
 if [ ! -f sql_files.txt ] || [ ! -s sql_files.txt ]; then
     echo "No SQL files found to validate."
     # Set empty validation result
-    echo "validation-result=" >> "$GITHUB_OUTPUT"
+    echo "sql-files-changed=false" >>"$GITHUB_OUTPUT"
     exit 0
 fi
+
+echo "sql-files-changed=true" >>"$GITHUB_OUTPUT"
--- .github/actions/sql-naming-convention-check/scripts/validate_sql_naming/validate_sql_naming.sh
+++ .github/actions/sql-naming-convention-check/scripts/validate_sql_naming/validate_sql_naming.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -Eeuox pipefail
+set -Eeuo pipefail
 
 if [[ $# -ne 1 ]]; then
   echo "Usage: $0 <sql_files_list.txt>"
