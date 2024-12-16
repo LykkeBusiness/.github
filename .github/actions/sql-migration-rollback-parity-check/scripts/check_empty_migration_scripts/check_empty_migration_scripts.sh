247cda11ed70d0828ff87ab0bdada4aac041e9ce
atarutin <andrey.tarutin@lykke-business.ch>
2024-12-16 15:43:52 +0300

    feat: add SQL Migration-Rollback Parity Check action with associated scripts

---
+++ .github/actions/sql-migration-rollback-parity-check/scripts/check_empty_migration_scripts/check_empty_migration_scripts.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+if [ ! -f migration_scripts.txt ] || [ ! -s migration_scripts.txt ]; then
+    echo "No migration script files found."
+    # Set empty validation result
+    echo "validation-result=" >> "$GITHUB_OUTPUT"
+    exit 0
+fi
---
+++ .github/actions/sql-migration-rollback-parity-check/scripts/find_migration_scripts/find_migration_scripts.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+# Directory to search for SQL scripts. Default is the current directory.
+SEARCH_DIR=${1:-.}
+
+MIGRATION_SCRIPTS=$(find "$SEARCH_DIR" -type f -name "\[MIGRATION\]*.sql")
+
+if [ -z "$MIGRATION_SCRIPTS" ]; then
+  exit 0
+fi
+
+echo "$MIGRATION_SCRIPTS" > migration_scripts.txt
