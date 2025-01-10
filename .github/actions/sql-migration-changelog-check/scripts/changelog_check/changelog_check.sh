8010a5dcc5d6bcb46edbcef391f1bc86a2044cc4
atarutin <andrey.tarutin@lykke-business.ch>
2025-01-10 11:15:40 +0300

    refactor: improve usage validation and streamline filename checks in changelog script

--- .github/actions/sql-migration-changelog-check/scripts/changelog_check/changelog_check.sh
+++ .github/actions/sql-migration-changelog-check/scripts/changelog_check/changelog_check.sh
@@ -1,45 +1,44 @@
 #!/usr/bin/env bash
 set -Eeuox pipefail
 
-if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 <migration_scripts.txt>"
+if [[ $# -lt 1 || $# -gt 2 ]]; then
+    echo "Usage: $0 <migration_scripts.txt> [search_dir]"
     exit 1
 fi
 
-MIGRATION_SCRIPTS_LIST=$1
+MIGRATION_SCRIPTS_LIST="$1"
+SEARCH_DIR="${2:-.}"
+
 if [[ ! -f $MIGRATION_SCRIPTS_LIST ]]; then
     echo "Error: File '$MIGRATION_SCRIPTS_LIST' does not exist."
     exit 1
 fi
 
-# Directory to search for CHANGELOG file. Default is the current directory.
-SEARCH_DIR=${2:-.}
-
-CHANGELOG_FILE_PATH=$(find "$SEARCH_DIR" -type f -name "CHANGELOG.md")
+CHANGELOG_FILE_PATH="$(find "$SEARCH_DIR" -type f -name "CHANGELOG.md" -print -quit)"
 
 if [ -z "$CHANGELOG_FILE_PATH" ]; then
     echo "Error: CHANGELOG.md file not found in the directory '$SEARCH_DIR'."
     exit 0
 fi
 
+echo "CHANGELOG file to be checked: $CHANGELOG_FILE_PATH"
+
 INVALID_FILES=()
 
 while IFS= read -r file; do
-    [[ -z "$file" ]] && continue
-
-    filename=$(basename "$file")
-
-    if [[ $filename =~ ^\[MIGRATION\] ]]; then
-        if ! grep -Fxq "### Migration: <<<$filename>>> [MANUAL]" "$CHANGELOG_FILE_PATH"; then
-            INVALID_FILES+=("$file")
-        fi
-    elif [[ $filename =~ ^\[ROLLBACK\] ]]; then
-        if ! grep -Fxq "### Rollback: <<<$filename>>>" "$CHANGELOG_FILE_PATH"; then
-            INVALID_FILES+=("$file")
-        fi
-    else
+    [[ -n "$file" ]] || continue
+    filename="$(basename "$file")"
+    case "$filename" in
+    "[MIGRATION]"*)
+        grep -Fxq "### Migration: <<<$filename>>> [MANUAL]" "$CHANGELOG_FILE_PATH" || INVALID_FILES+=("$file")
+        ;;
+    "[ROLLBACK]"*)
+        grep -Fxq "### Rollback: <<<$filename>>>" "$CHANGELOG_FILE_PATH" || INVALID_FILES+=("$file")
+        ;;
+    *)
         INVALID_FILES+=("$file")
-    fi
+        ;;
+    esac
 done <"$MIGRATION_SCRIPTS_LIST"
 
 if ((${#INVALID_FILES[@]} > 0)); then
