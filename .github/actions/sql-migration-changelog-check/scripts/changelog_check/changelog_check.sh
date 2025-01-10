be9ee7f22fafbdfd4a5cb8957d10ef27801236cd
atarutin <andrey.tarutin@lykke-business.ch>
2025-01-10 11:21:54 +0300

    fix: update error handling and improve script usage validation in changelog check script

--- .github/actions/sql-migration-changelog-check/scripts/changelog_check/changelog_check.sh
+++ .github/actions/sql-migration-changelog-check/scripts/changelog_check/changelog_check.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -Eeuox pipefail
+set -Eeuo pipefail
 
 if [[ $# -lt 1 || $# -gt 2 ]]; then
     echo "Usage: $0 <migration_scripts.txt> [search_dir]"
@@ -8,23 +8,18 @@ fi
 
 MIGRATION_SCRIPTS_LIST="$1"
 SEARCH_DIR="${2:-.}"
-
 if [[ ! -f $MIGRATION_SCRIPTS_LIST ]]; then
     echo "Error: File '$MIGRATION_SCRIPTS_LIST' does not exist."
     exit 1
 fi
 
 CHANGELOG_FILE_PATH="$(find "$SEARCH_DIR" -type f -name "CHANGELOG.md" -print -quit)"
-
 if [ -z "$CHANGELOG_FILE_PATH" ]; then
     echo "Error: CHANGELOG.md file not found in the directory '$SEARCH_DIR'."
     exit 0
 fi
 
-echo "CHANGELOG file to be checked: $CHANGELOG_FILE_PATH"
-
 INVALID_FILES=()
-
 while IFS= read -r file; do
     [[ -n "$file" ]] || continue
     filename="$(basename "$file")"
